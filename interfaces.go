1ab4c0bef5c431d9cb3342ff01586e499d133912
Davide M. Puggioni <davide.puggioni@suse.com>
2026-06-11 17:06:16 +0200

    removed interface.go - dead code

--- interfaces.go
+++
@@ -1,4 +0,0 @@
-// interfaces.go - Now empty: interface abstractions removed.
-// They were unused wrappers around direct function calls.
-// The actual functions (scanFiles, anyFileContains, etc.) remain in utils.go.
-package main
